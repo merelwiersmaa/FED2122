4f8861722cea94c7054bc8ad95f499b459f1ffa2
merelwiersmaa <83574654+merelwiersmaa@users.noreply.github.com>
2021-11-11 12:30:18 +0100

    definitieve html, css en js.
    comments toegevoegd, nu klaar.

--- script2.js
+++ script2.js
@@ -1,7 +1,13 @@
+// hieronder wordt de variabele en functie aan gemaakt voor het switchen van voorkant naar zijkant foto
+
+// de variabele krijgt de .zijkant aangewezen, en dat is het knopje waar op gedrukt wordt
 var zijkant = document.querySelector(".zijkant");
 
+// deze variabele krijgt de foto zelf toegewezen
 var voorkantfoto = document.querySelector(".voorkantfoto");
 
+// bij deze functie wordt dus de variabele voorkantfoto, veranderd van source -> "watch belowheader..." en veranderd de afbeelding dus
+// dit gebeurt wanneer de variabele zijkant, wordt aangeklikt
 function zijkantfoto() {
     voorkantfoto.src = "watch_belowheader/WATCH2.png"
     }
